1f36a27b63e12597d86b07acb20fb55962e321e9
时小满 <xiaomanshi@clubfactory.com>
2020-04-27 18:01:16 +0800

    test: test

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import * as React from 'react'
 import ReactDOM from 'react-dom'
 
-// add test
 import App from '@/components/App'
 
 ReactDOM.render(<App />, document.querySelector('#app'), () => {})
