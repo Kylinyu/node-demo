4db9f91292131f291a89968bbba3a3b950986c0c
时小满 <xiaomanshi@clubfactory.com>
2020-04-27 17:58:14 +0800

    fix

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import ReactDOM from 'react-dom'
 
+// add test
 import App from '@/components/App'
 
 ReactDOM.render(<App />, document.querySelector('#app'), () => {})
